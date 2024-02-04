4003abcb71b50234d99aa1573a958dd5f180667c
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-05 00:43:54 +0100

    task2 => we have added the get and count methode yarbi tkhdem

--- test_get_count.py
+++ test_get_count.py
@@ -3,9 +3,10 @@
 """
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 
 print("All objects: {}".format(storage.count()))
-print("State objects: {}".format(storage.count(State)))
+print("Amenity objects: {}".format(storage.count(Amenity)))
 
-first_state_id = list(storage.all(State).values())[0].id
-print("First state: {}".format(storage.get(State, first_state_id)))
+first_state_id = list(storage.all(Amenity).values())[0].id
+print("First amenity: {}".format(storage.get(Amenity, first_state_id)))

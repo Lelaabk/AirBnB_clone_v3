13366029fe7881f1407eaad8f17499b084ba854f
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-05 21:55:04 +0100

    task 13 => adding amenties endpoint and handeling its relationship with place

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""docs"""
+"""sharing app_views Blueprint"""
+
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+app_views = Blueprint('app_views', __name__)
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -11,3 +10,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *

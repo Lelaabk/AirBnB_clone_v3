09e6ba7ac798c9abfcfca286bf135e12aafef4e8
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-05 01:08:30 +0100

    task 4 => fixing count to retruve the count of other classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.city import City
+from models.user import User
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -14,12 +20,18 @@ def status():
 @app_views.route("/stats", strict_slashes=False, methods=["GET"])
 def stats():
     """Retrieves the number of each object type"""
-    stats_dict = {
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User')
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    users = storage.count(User)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+
+    return {
+        "amenities": amenities,
+        "cities": cities,
+        "users": users,
+        "places": places,
+        "reviews": reviews,
+        "states": states
     }
-    return jsonify(stats_dict)

83fcb1dd34d03aec78941800f7af0fea796b3cb1
Lelaabk <lolabkari@gmail.com>
2024-02-05 03:56:51 -0800

    Fix new lines

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,5 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
+
+app_views.register_blueprint(states.app_views)

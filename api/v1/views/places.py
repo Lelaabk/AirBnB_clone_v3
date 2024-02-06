48b0343a5aee5a86440630341f929e0e8462dd8f
Lelaabk <lolabkari@gmail.com>
2024-02-05 17:59:26 -0800

    Search Places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -92,3 +92,58 @@ def update_place(place_id):
         return jsonify(place.to_dict()), 200
     else:
         abort(404)
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def places_search():
+    """ Retrieves Place object based on provided JSON search critea """
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+
+    if data and len(data):
+        states = data.get('states', None)
+        cities = data.get('cities', None)
+        amenities = data.get('amenities', None)
+
+    if data is None or len(data) is None or (states is None and cities is None
+                                             and amenities is None):
+        places = storage.all(Place).values()
+        list_places = []
+        for place in places:
+            list_places.append(place.to_dict())
+        return jsonify(list_places)
+    list_places = []
+
+    if states:
+        states_obj = [storage.get(State, s_id) for s_id in states]
+        for state in states_obj:
+            if state:
+                for city in state.cities:
+                    if city:
+                        for place in city.places:
+                            list_places.append(place)
+
+    if cities:
+        city_obj = [storage.get(City, c_id) for c_id in cities]
+        for city in city_obj:
+            if city:
+                for place in city.places:
+                    if place not in list_places:
+                        list_places.append(place)
+
+    if amenities:
+        if not list_places:
+            list_places = storage.all(Place).values()
+        amenities_obj = [storage.get(Amenity, a_id) for a_id in amenities]
+
+        list_places = [place for place in list_places if
+                       all([am in place.amenities for am in amenities_obj])]
+
+    places = []
+    for plc in list_places:
+        dc = plc.to_dict()
+        dc.pop('amenities', None)
+        places.append(dc)
+
+    return jsonify(places)
